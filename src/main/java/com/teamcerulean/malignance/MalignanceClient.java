39c66aab3e8a8eb0bbefb0d3d5b2c222a76b9b62
kibblewibble <jacksonbkilgore@gmail.com>
2024-11-19 18:09:04 -0500

    Initial commit

---
+++ src/main/java/com/teamcerulean/malignance/MalignanceClient.java
@@ -0,0 +1,10 @@
+package com.teamcerulean.malignance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MalignanceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
